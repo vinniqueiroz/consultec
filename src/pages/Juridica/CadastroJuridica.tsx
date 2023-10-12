234dd31a61a371a2e2e5df612bbecfce2c673ca8
Hugovco511 <hugovco511@hotmail>
2023-10-12 11:38:46 -0300

    Refactor: Cadastro cliente juridico com TextInput

--- src/pages/Juridica/CadastroJuridica.tsx
+++ src/pages/Juridica/CadastroJuridica.tsx
@@ -1,81 +1,110 @@
-import { Button, Flex, Input, Switch } from "@mantine/core";
+import { Button, Flex, InputBase, Switch, TextInput } from "@mantine/core";
 import { isNotEmpty, useForm } from "@mantine/form";
-import style from "./Cadastro.module.css";
-import cadastraCliente from "../../services/client/cadastraClientes";
-import { InputBase } from "@mantine/core";
+import { useMediaQuery } from "@mantine/hooks";
 import { IMaskInput } from "react-imask";
+import cadastraCliente from "../../services/client/cadastraClientes";
+import style from "./Cadastro.module.css";
 
-export default function CadastroJuridica() {
-  interface FormValues {
-    bairro: string;
-    cep: string;
-    cgc: string;
-    cidade: string;
-    contrato: string;
-    fiscal: string;
-    pessoal: string;
-    contabil: string;
-    email: string;
-    estado: string;
-    fantasia: string;
-    logradouro: string;
-    numero: string;
-    passsefaz: string;
-    ramoatividade: string;
-    razaosocial: string;
-    telefone: string;
-    usursefaz: string;
-    cnae: string;
-    pais: string;
-  }
-
-  const form = useForm<FormValues>({
+export default function CadastroFisica() {
+  const isMobile = useMediaQuery("(max-width: 50em)");
+  const form = useForm({
     initialValues: {
-      bairro: "",
-      cep: "",
-      cgc: "",
-      cidade: "",
-      contrato: "S" || "N",
-      fiscal: "",
-      pessoal: "",
-      contabil: "",
-      email: "",
-      estado: "",
-      fantasia: "",
-      logradouro: "",
-      numero: "",
-      passsefaz: "",
-      ramoatividade: "",
       razaosocial: "",
+      cgc: "",
+      fantasia: "",
       telefone: "",
+      email: "",
+      cep: "",
+      cidade: "",
+      estado: "",
+      numero: "",
+      bairro: "",
+      ramodeatividade: "",
+      logradouro: "",
       usursefaz: "",
-      cnae: "",
+      passsefaz: "",
+      contrato: "",
       pais: "",
+      cnae: "",
     },
     validate: {
-      bairro: isNotEmpty("Bairro"),
-      cep: isNotEmpty("CEP"),
-      cgc: isNotEmpty("CNPJ"),
-      cidade: isNotEmpty("Cidade"),
-      email: isNotEmpty("E-mail"),
-      estado: isNotEmpty("Estado"),
-      fantasia: isNotEmpty("Fantasia"),
-      logradouro: isNotEmpty("Logradouro"),
-      numero: isNotEmpty("Número"),
-      passsefaz: isNotEmpty("Senha"),
-      ramoatividade: isNotEmpty("Ramo de Atividade"),
-      razaosocial: isNotEmpty("Razão Social"),
-      telefone: isNotEmpty("Telefone"),
-      usursefaz: isNotEmpty("Usuário"),
-      cnae: isNotEmpty("Cnae"),
-      pais: isNotEmpty("Pais"),
+      razaosocial: isNotEmpty("O campo de razão social não pode ser vazio"),
+      cgc: isNotEmpty("O campo de cgc não pode ser vazio"),
+      fantasia: isNotEmpty("O campo de nome fantasia não pode ser vazio"),
+      telefone: isNotEmpty("O campo de telefone não pode ser vazio"),
+      email: isNotEmpty("O campo de email não pode ser vazio"),
+      cidade: isNotEmpty("O campo de cidade não pode ser vazio"),
+      cep: isNotEmpty("O campo de cep não pode ser vazio"),
+      estado: isNotEmpty("O campo de estado não pode ser vazio"),
+      bairro: isNotEmpty("O campo de bairro não pode ser vazio"),
+      numero: isNotEmpty("O campo de numero não pode ser vazio"),
+      ramodeatividade: isNotEmpty("O campo de atividades não pode ser vazio"),
+      logradouro: isNotEmpty("O campo de logradouro não pode ser vazio"),
+      usursefaz: isNotEmpty("O campo de usuario não pode ser vazio"),
+      passsefaz: isNotEmpty("O campo de senha não pode ser vazio"),
+      contrato: isNotEmpty("O campo de contrato não pode ser vazio"),
+      pais: isNotEmpty("O campo de país não pode ser vazio"),
+      cnae: isNotEmpty("O campo de cnae não pode ser vazio"),
     },
   });
 
-  const cadastra = async (values: FormValues) => {
+  const cadastra = async ({
+    razaosocial,
+    cgc,
+    fantasia,
+    telefone,
+    email,
+    cep,
+    cidade,
+    estado,
+    bairro,
+    numero,
+    ramodeatividade,
+    logradouro,
+    usursefaz,
+    passsefaz,
+    contrato,
+    pais,
+    cnae,
+  }: {
+    razaosocial: string;
+    cgc: string;
+    fantasia: string;
+    telefone: string;
+    email: string;
+    cep: string;
+    cidade: string;
+    estado: string;
+    bairro: string;
+    numero: string;
+    ramodeatividade: string;
+    logradouro: string;
+    usursefaz: string;
+    passsefaz: string;
+    contrato: string;
+    pais: string;
+    cnae: string;
+  }) => {
     try {
-      await cadastraCliente(values);
-
+      await cadastraCliente({
+        razaosocial,
+        cgc,
+        fantasia,
+        telefone,
+        email,
+        cep,
+        cidade,
+        estado,
+        bairro,
+        numero,
+        ramodeatividade,
+        logradouro,
+        usursefaz,
+        passsefaz,
+        contrato,
+        pais,
+        cnae,
+      });
       form.reset();
     } catch (error) {
       console.log(error);
@@ -84,192 +113,158 @@ export default function CadastroJuridica() {
   return (
     <div>
       <form onSubmit={form.onSubmit(cadastra)}>
-        <Flex justify="center" align="start">
-          <Flex justify="center" align="center" direction="column" rowGap="xl">
-            <Flex columnGap="5em" rowGap="xs" direction="row" wrap="wrap">
-              <Flex justify="center" align="start" gap="xs" direction="column">
-                <Flex>
-                  <Switch
-                    label="Contrato"
-                    size="sm"
-                    {...form.getInputProps("contrato")}
-                  />
-                </Flex>
-                <div>
-                  <label htmlFor="Razão Social">Razão Social</label>
-                  <Input
-                    name="Razão Social"
-                    placeholder="Informe a razão social"
-                    className={style.inputCamp}
-                    {...form.getInputProps("razaosocial")}
-                  />
-                </div>
-                <div>
-                  <label htmlFor="CPF">CNPJ</label>
-                  <InputBase<any>
-                    name="CNPJ"
-                    component={IMaskInput}
-                    mask="00.000.000/0000-00"
-                    placeholder="Informe o CNPJ"
-                    className={style.inputCamp}
-                    {...form.getInputProps("cgc")}
-                  />
-                </div>
-                <div>
-                  <label htmlFor="Ramo de Atividade">Ramo de Atividade</label>
-                  <Input
-                    name="Ramo de Atividade"
-                    placeholder="Informe o ramo de atividade"
-                    className={style.inputCamp}
-                    {...form.getInputProps("ramoatividade")}
-                  />
-                </div>
-                <div>
-                  <label htmlFor="CEP">CEP</label>
-                  <InputBase<any>
-                    name="CEP"
-                    component={IMaskInput}
-                    mask="00000-000"
-                    placeholder="Informe o CEP"
-                    className={style.inputCamp}
-                    {...form.getInputProps("cep")}
-                  />
-                </div>
-                <div>
-                  <label htmlFor="Bairro">Bairro</label>
-                  <Input
-                    name="Bairro"
-                    placeholder="Informe o Bairro"
-                    className={style.inputCamp}
-                    {...form.getInputProps("bairro")}
-                  />
-                </div>
-                <div>
-                  <label htmlFor="logradouro">logradouro</label>
-                  <Input
-                    name="logradouro"
-                    placeholder="Informe o logradouro"
-                    className={style.inputCamp}
-                    {...form.getInputProps("logradouro")}
-                  />
-                </div>
-                <div>
-                  <label htmlFor="cnae">CNAE</label>
-                  <Input
-                    name="cnae"
-                    placeholder="Informe o cnae"
-                    className={style.inputCamp}
-                    {...form.getInputProps("cnae")}
-                  />
-                </div>
-                <div>
-                  <label htmlFor="Usuário Sefaz">Usuário Sefaz</label>
-                  <Input
-                    name="Usuário Sefaz"
-                    placeholder="Login"
-                    className={style.inputCamp}
-                    {...form.getInputProps("usursefaz")}
-                  />
-                </div>
-              </Flex>
-              <Flex justify="center" align="center" gap="xs" direction="column">
-                <Flex align="center" columnGap="5.4em" direction="row">
-                  <Switch
-                    label="Fiscal"
-                    size="sm"
-                    {...form.getInputProps("fiscal")}
-                  />
-                  <Switch
-                    label="Pessoal"
-                    size="sm"
-                    {...form.getInputProps("pessoal")}
-                  />
-                  <Switch
-                    label="Contábil"
-                    size="sm"
-                    {...form.getInputProps("contabil")}
-                  />
-                </Flex>
-                <div>
-                  <label htmlFor="Nome Fantasia">Nome Fantasia</label>
-                  <Input
-                    name="Nome Fantasia"
-                    placeholder="Informe o nome fantasia"
-                    className={style.inputCamp}
-                    {...form.getInputProps("fantasia")}
-                  />
-                </div>
-                <div>
-                  <label htmlFor="Telefone">Telefone</label>
-                  <InputBase<any>
-                    name="Telefone"
-                    component={IMaskInput}
-                    mask="+00 (00) 00000-0000"
-                    placeholder="Informe o telefone"
-                    className={style.inputCamp}
-                    {...form.getInputProps("telefone")}
-                  />
-                </div>
-                <div>
-                  <label htmlFor="E-mail">E-mail</label>
-                  <Input
-                    name="E-mail"
-                    placeholder="Informe o e-mail"
-                    className={style.inputCamp}
-                    {...form.getInputProps("email")}
-                  />
-                </div>
-                <div>
-                  <label htmlFor="Cidade">Cidade</label>
-                  <Input
-                    name="Cidade"
-                    placeholder="Informe o cidade"
-                    className={style.inputCamp}
-                    {...form.getInputProps("cidade")}
-                  />
-                </div>
-                <div>
-                  <label htmlFor="Estado">Estado</label>
-                  <Input
-                    name="Estado"
-                    placeholder="Informe o estado"
-                    className={style.inputCamp}
-                    {...form.getInputProps("estado")}
-                  />
-                </div>
-                <div>
-                  <label htmlFor="pais">País</label>
-                  <Input
-                    name="pais"
-                    placeholder="Informe o País"
-                    className={style.inputCamp}
-                    {...form.getInputProps("pais")}
-                  />
-                </div>
-                <div>
-                  <label htmlFor="Numero">Numero</label>
-                  <Input
-                    name="Numero"
-                    placeholder="Informe o numero"
-                    className={style.inputCamp}
-                    {...form.getInputProps("numero")}
-                  />
-                </div>
-                <div>
-                  <label htmlFor="Senha Sefaz">Senha Sefaz</label>
-                  <Input
-                    name="Senha Sefaz"
-                    placeholder="Senha"
-                    className={style.inputCamp}
-                    {...form.getInputProps("passsefaz")}
-                  />
-                </div>
-              </Flex>
+        <Flex direction="column" align="center" p="xs">
+          <Flex
+            wrap="wrap"
+            justify="space-between"
+            direction={isMobile ? "column" : "row"}
+            className={style.swicths}
+          >
+            <Flex justify={isMobile ? "center" : "space-between"}>
+              <Switch.Group
+                label="Contrato"
+                withAsterisk
+                size="sm"
+                {...form.getInputProps("contrato")}
+              >
+                <Switch value="Contrato" />
+              </Switch.Group>
             </Flex>
-            <Button fullWidth className="botao" type="submit">
-              Salvar
-            </Button>
+            <Flex
+              align="center"
+              columnGap="5.4em"
+              justify={isMobile ? "center" : "space-between"}
+            >
+              <Switch.Group label="Fiscal" size="sm">
+                <Switch value="fiscal" />
+              </Switch.Group>
+              <Switch.Group label="Pessoal" size="sm">
+                <Switch value="pessoal" />
+              </Switch.Group>
+              <Switch.Group label="Contábil" size="sm">
+                <Switch value="contabil" />
+              </Switch.Group>
+            </Flex>
           </Flex>
+          <Flex
+            justify={isMobile ? "center" : "space-between"}
+            wrap="wrap"
+            gap="xs"
+          >
+            <TextInput
+              label="Razão social "
+              placeholder="Informe a razão social"
+              className={style.inputCamp}
+              {...form.getInputProps("razaosocial")}
+            />
+
+            <TextInput
+              label="Nome fantasia"
+              placeholder="Informe o nome fantasia"
+              className={style.inputCamp}
+              {...form.getInputProps("fantasia")}
+            />
+            <InputBase<any>
+              label="CNPJ"
+              component={IMaskInput}
+              mask="000.000.000-00"
+              placeholder="Informe o CNPJ"
+              className={style.inputCamp}
+              {...form.getInputProps("cgc")}
+            />
+            <InputBase<any>
+              label="Telefone"
+              component={IMaskInput}
+              mask="+00 (00) 00000-0000"
+              placeholder="Informe o telefone"
+              className={style.inputCamp}
+              {...form.getInputProps("telefone")}
+            />
+            <TextInput
+              label="E-mail"
+              placeholder="Informe o e-mail"
+              className={style.inputCamp}
+              {...form.getInputProps("email")}
+            />
+            <TextInput
+              label="CNAE"
+              placeholder="Informe o cnae"
+              className={style.inputCamp}
+              {...form.getInputProps("cnae")}
+            />
+
+            <TextInput
+              label="Ramo de Atividade"
+              placeholder="Informe o ramo de atividade"
+              className={style.inputCamp}
+              {...form.getInputProps("ramodeatividade")}
+            />
+
+            <InputBase<any>
+              label="CEP"
+              component={IMaskInput}
+              mask="00000-000"
+              placeholder="Informe o CEP"
+              className={style.inputCamp}
+              {...form.getInputProps("cep")}
+            />
+            <TextInput
+              label="Cidade"
+              placeholder="Informe o cidade"
+              className={style.inputCamp}
+              {...form.getInputProps("cidade")}
+            />
+
+            <TextInput
+              label="Logradouro"
+              placeholder="Informe o logradouro"
+              className={style.inputCamp}
+              {...form.getInputProps("logradouro")}
+            />
+            <TextInput
+              label="Bairro"
+              placeholder="Informe o Bairro"
+              className={style.inputCamp}
+              {...form.getInputProps("bairro")}
+            />
+
+            <TextInput
+              label="Estado"
+              placeholder="Informe o estado"
+              className={style.inputCamp}
+              {...form.getInputProps("estado")}
+            />
+
+            <TextInput
+              label="País"
+              placeholder="Informe o País"
+              className={style.inputCamp}
+              {...form.getInputProps("pais")}
+            />
+
+            <TextInput
+              label="Número"
+              placeholder="Informe o numero"
+              className={style.inputCamp}
+              {...form.getInputProps("numero")}
+            />
+
+            <TextInput
+              label="Usuário Sefaz"
+              placeholder="Login"
+              className={style.inputCamp}
+              {...form.getInputProps("usursefaz")}
+            />
+            <TextInput
+              label="Senha Sefaz"
+              placeholder="Senha"
+              className={style.inputCamp}
+              {...form.getInputProps("passsefaz")}
+            />
+          </Flex>
+          <Button mt="xl" className={style.salvar}>
+            Salvar
+          </Button>
         </Flex>
       </form>
     </div>
